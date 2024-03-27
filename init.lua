dc2788b051cada4d1e8fc50ad398f66c0b3708a0
pluviolithic <e-mckanna@onu.edu>
2024-03-27 12:21:23 -0400

    Add linting and formatting
    - Add selene.toml and neovim.yml for linting
    - Add .stylua.toml for formatting
    
    Both use the conventions set forth in the neovim repositories

--- init.lua
+++ init.lua
@@ -1,21 +1,21 @@
-require("mappings")
-require("autocmds")
+require 'mappings'
+require 'autocmds'
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
-require("gruvbox").setup()
-require("treesitter")
+require('lazy').setup 'plugins'
+require('gruvbox').setup()
+require 'treesitter'
 
-vim.cmd("colorscheme gruvbox")
+vim.cmd 'colorscheme gruvbox'
