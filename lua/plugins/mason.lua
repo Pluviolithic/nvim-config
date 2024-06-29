dc806cbe9d1a4d583310e87e11e2a25b47e0f4e4
Eric McKanna <Eric.McKanna@is4s.com>
2024-06-28 23:51:27 -0400

    Update luau-lsp configurations

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,28 +1,48 @@
 return {
-	"williamboman/mason.nvim",
+	'williamboman/mason.nvim',
 	dependencies = {
-		{ "williamboman/mason-lspconfig.nvim" },
-		{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
-		{ "neovim/nvim-lspconfig" },
+		{ 'williamboman/mason-lspconfig.nvim' },
+		{ 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
+		{ 'neovim/nvim-lspconfig' },
 	},
 	config = function()
-		local lsp_zero = require("lsp-zero")
+		local lsp_zero = require 'lsp-zero'
 
 		lsp_zero.on_attach(function(_, bufnr)
 			-- see :help lsp-zero-keybindings
 			-- to learn the available actions
-			lsp_zero.default_keymaps({ buffer = bufnr })
+			lsp_zero.default_keymaps { buffer = bufnr }
 		end)
 
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = { "luau_lsp" },
+		require('mason').setup()
+		require('mason-lspconfig').setup {
+			ensure_installed = { 'luau_lsp' },
 			handlers = {
 				lsp_zero.default_setup,
 			},
-		})
-		require("mason-lspconfig").setup_handlers({
-			luau_lsp = require("lsp.luau"),
-		})
+		}
+		require('mason-lspconfig').setup_handlers {
+			luau_lsp = function()
+				require('luau-lsp').setup {
+					plugin = {
+						enabled = true,
+						port = 3667,
+					},
+					sourcemap = {
+						enabled = true,
+						autogenerate = true, -- automatic generation when the server is attached
+						rojo_project_file = 'default.project.json',
+					},
+					platform = {
+						type = 'roblox',
+					},
+					completion = {
+						imports = {
+							enabled = true,
+						},
+					},
+				}
+			end,
+		}
 	end,
 }
